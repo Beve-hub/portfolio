7f08df01cf5161b3d1096913c18033fe2d172934
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-11-30 20:17:25 -0700

    ten

--- src/Page/Project.jsx
+++ src/Page/Project.jsx
@@ -21,7 +21,7 @@ const Project = () => {
     <section className="h-screen flex px-10 md:flex-row grid-col-2 items-center justify-center overflow-hidden">
       <div  className=' max-w-[1640px] md:mx-auto grid-cols-2 gap-6 px-20 justify-center  cursor-pointer '> 
       <h1 className='flex justify-center font-bold text-white text-2xl'>Our Amazing Project</h1>
-      <div className='flex gap-6 justify-center my-4'>
+      <div className='flex gap-6 justify-center my-6'>
         <div className=' bg-black text-white px-8 p-2  flex justify-center  rounded-lg'>
           <p >UIUX</p>
         </div>
@@ -33,7 +33,7 @@ const Project = () => {
         </div> 
       </div>
 
-      <div className=' max-w-[1640px] md:mx-auto grid-col grid-cols-2 gap-10 px-20 justify-center w-[80rem]  cursor-pointer '> 
+      <div className=' max-w-[1640px] md:mx-auto grid-col grid-cols-2 gap-10 px-20 justify-center w-[80rem]  cursor-pointer pb-10'> 
        
       <Swiper
         slidesPerView={3}
