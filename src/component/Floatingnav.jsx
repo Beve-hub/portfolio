d16b199de7d276acea2540bd1be4710992fb7399
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-02-03 20:17:04 +0000

    dfdf

--- src/component/Floatingnav.jsx
+++ src/component/Floatingnav.jsx
@@ -6,13 +6,13 @@ import { NavLink} from 'react-router-dom';
 const Floatingnav = () => {
     const [active, setActive] = useState(0);
     return (
-     <div className='absolute bottom-0 w-full h-[4rem] py-2 md:hidden sm:backdrop-opacity-10  bg-[#142149] backdrop-opacity-30 '>
-        <ul className=' flex justify-around items-center' >
+     <div className='absolute bottom-0 w-full h-[4rem] py-2 md:hidden sm:backdrop-opacity-10  bg-[#142149] backdrop-opacity-30 z-99'>
+        <ul className=' flex justify-around items-center z-10' >
             {float.map(({name,path,icon},index) => {
           return (
             <li key={index}  className='grid justify-center '>
                 <NavLink to={path} onClick={() => setActive(index)} className='grid justify-center ' >
-                   <span  className={`flex justify-center cursor-pointer duration-500 ${index === active && "bg-[royalblue] border-4 border-[#2B3752] p-4 -mt-10 rounded-full"}`}>{icon}</span>
+                   <span  className={`flex justify-center cursor-pointer z-99 duration-500 ${index === active && "bg-[royalblue] border-4 border-[#2B3752] p-4 -mt-10 rounded-full"}`}>{icon}</span>
                     <span className={`${active === index ? "translate-y-2 flex justify-center text-md font-regular duration-700 opacity-100" : "opacity-0 translate-y-10"}`}>{name}</span>
                 </NavLink>
             </li>
